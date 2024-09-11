9f04e152f48f93815193c65e21132c02b2f98c44
TR4E <trae.rohleder@outlook.com>
2024-09-11 14:41:50 +1000

    Changes (Mostly Weapon Framework)

--- src/main/java/me/trae/champions/weapon/types/ChampionsPvPWeapon.java
+++ src/main/java/me/trae/champions/weapon/types/ChampionsPvPWeapon.java
@@ -2,13 +2,19 @@ package me.trae.champions.weapon.types;
 
 import me.trae.champions.Champions;
 import me.trae.champions.weapon.WeaponManager;
-import me.trae.core.weapon.Weapon;
+import me.trae.core.weapon.data.WeaponData;
+import me.trae.core.weapon.types.CustomItem;
 import org.bukkit.inventory.ItemStack;
 
-public abstract class ChampionsPvPWeapon extends Weapon<Champions, WeaponManager> {
+public abstract class ChampionsPvPWeapon extends CustomItem<Champions, WeaponManager, WeaponData> {
 
     public ChampionsPvPWeapon(final WeaponManager manager, final ItemStack itemStack) {
         super(manager, itemStack);
+    }
+
+    @Override
+    public Class<WeaponData> getClassOfData() {
+        return WeaponData.class;
     }
 
     @Override
