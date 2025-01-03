843f192088eaf896a144ef4973517478f5e54e9d
TR4E <trae.rohleder@outlook.com>
2025-01-03 21:12:53 +1100

    Changes

--- src/main/java/me/trae/champions/world/modules/HandleChampionsWeaponDurability.java
+++ src/main/java/me/trae/champions/world/modules/HandleChampionsWeaponDurability.java
@@ -3,12 +3,16 @@ package me.trae.champions.world.modules;
 import me.trae.api.damage.events.weapon.WeaponDurabilityEvent;
 import me.trae.champions.Champions;
 import me.trae.champions.world.WorldManager;
+import me.trae.core.config.annotations.ConfigInject;
 import me.trae.core.framework.types.frame.SpigotListener;
 import me.trae.core.utility.enums.WeaponMaterialType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 
 public class HandleChampionsWeaponDurability extends SpigotListener<Champions, WorldManager> {
+
+    @ConfigInject(type = Integer.class, path = "Gold-Durability-Percentage", defaultValue = "25")
+    private int goldDurabilityPercentage;
 
     public HandleChampionsWeaponDurability(final WorldManager manager) {
         super(manager);
@@ -28,7 +32,9 @@ public class HandleChampionsWeaponDurability extends SpigotListener<Champions, W
             return;
         }
 
-        if (Math.random() <= 0.52D) {
+        final double chance = this.goldDurabilityPercentage / 100.0D;
+
+        if (Math.random() <= chance) {
             return;
         }
 
